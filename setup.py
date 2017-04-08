1ba797217fe109db5a3c3d721025f43a11664906
lqmanh <luongquangmanh85@gmail.com>
2017-04-08 20:44:46 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='classer',
+    version='0.1dev',
+    author='Luong Quang Manh',
+    license='MIT',
+    install_requires=('Click',),
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': ('classer=sample.main:cli',),
+    }
+)
