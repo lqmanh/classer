ddc96d3bc1201d501a04cb571f074569cff3747e
lqmanh <luongquangmanh85@gmail.com>
2017-06-07 22:54:53 +0700

    Add undo command

--- tests/test_reverseclassifier.py
+++ tests/test_reverseclassifier.py
@@ -7,18 +7,19 @@ def test_classify(tmpdir):
     subdir = tmpdir.mkdir('subdir')
     filepath1 = subdir.join('file1.md')
     filepath1.write('')
-    filepath2 = subdir.join('file2.txt')
+    filepath2 = tmpdir.join('file2.txt')
     filepath2.write('')
+    filepath3 = subdir.join('file2.txt')
+    filepath3.write('')
 
-    src_dir = os.path.abspath(tmpdir)
-    dst_dir = os.path.join(tmpdir, 'subdir')
+    src_dir = os.path.abspath(tmpdir)  # old src dir, new dst dir
+    dst_dir = os.path.join(tmpdir, 'subdir')  # old dst dir, new src dir
     lastrun_path = tmpdir.join('lastrun.txt')
     lastrun_path.write(f'Moved {src_dir}/file1.md to {dst_dir}/file1.md\n'
                        f'Moved {src_dir}/file2.txt to {dst_dir}/file2.txt\n')
 
     with open(lastrun_path) as f:
-        worker = ReverseClassifier(f)
+        worker = ReverseClassifier(f, autoclean=True, duplicate='rename')
         worker.classify()
 
-    assert set(os.listdir(tmpdir)) == {'subdir', 'lastrun.txt',
-                                       'file1.md', 'file2.txt'}
+    assert set(os.listdir(tmpdir)) == {'lastrun.txt', 'file1.md', 'file2.txt', 'file2 (2).txt'}
