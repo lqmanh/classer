4dc2bb7e2973f39dabb267fb8d6782fe61fe36a9
lqmanh <luongquangmanh85@gmail.com>
2017-06-23 21:38:54 +0700

    Change data dir to ~/.local/share/classer

--- sample/utils.py
+++ sample/utils.py
@@ -5,7 +5,7 @@ def open_lastrun_file(mode='r'):
     '''Open and return lastrun file.'''
 
     # path to data directory
-    data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+    data_dir = os.path.expanduser('~/.local/share/classer/')
     os.makedirs(data_dir, exist_ok=True)
 
     # path to lastrun file
